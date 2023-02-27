a569f5173307cca6ec27053fba661bf3afbba589
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-02-27 15:44:34 +0000

    Implements functions to generateTechUsed(), generateBadgeLogoLabel() and generateBadgeURL()

--- utils/questions.js
+++ utils/questions.js
@@ -22,103 +22,108 @@ const confirmInstructions = [
 const questions = [
   {
     type: "input",
-    message: "Title of the project:",
-    name: "title",
+    message: "Please insert all tech used separated by a comma",
+    name: "tech",
   },
-  {
-    type: "input",
-    message: "Description: (what is it about?) ",
-    name: "description",
-  },
-  {
-    type: "confirm",
-    message: "Do you already have an image preview for this application?",
-    name: "hasImagePreview",
-  },
-  {
-    type: "input",
-    message: "Path or URL of the image: ",
-    name: "imageURL",
-    when(answers) {
-      return answers.hasImagePreview;
-    },
-  },
-  {
-    type: "input",
-    message: "User story:",
-    name: "userStory",
-  },
-  {
-    type: "confirm",
-    message: "Are there any installation requirements for this application?",
-    name: "hasInstallationRequirements",
-  },
-  {
-    type: "input",
-    message: "Installation requirements: ",
-    name: "installation",
-    when(answers) {
-      return answers.hasInstallationRequirements;
-    },
-  },
-  {
-    type: "confirm",
-    message: "Do you provide tests for this application?",
-    name: "hasTests",
-  },
-  {
-    type: "input",
-    message: "How to run the tests? ",
-    name: "tests",
-    when(answers) {
-      return answers.hasTests;
-    },
-  },
-  {
-    type: "confirm",
-    message:
-      "Do you want to add a credits section to list the resources and people that helped you throughout the project?",
-    name: "hasCredits",
-  },
-  {
-    type: "input",
-    message: "Credits: ",
-    name: "credits",
-    when(answers) {
-      return answers.hasCredits;
-    },
-  },
-  {
-    type: "input",
-    message: "How can other developer contribute to the project? ",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "Email address: ",
-    name: "emailAddress",
-    validate: isValidEmail,
-  },
-  {
-    type: "input",
-    message: "Github username: ",
-    name: "userName",
-    validate: isValidUserName,
-  },
-  {
-    type: "confirm",
-    message: "Do you use a license for this project?",
-    name: "hasLicense",
-  },
-  {
-    type: "list",
-    message: "Please select the license: ",
-    name: "licenseChoice",
-    choices: ["MIT License", "Apache 2.0 License", "The Unlicense"],
-    when(answers) {
-      return answers.hasLicense;
-    },
-  },
+  // {
+  //   type: "input",
+  //   message: "Title of the project:",
+  //   name: "title",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Description: (what is it about?) ",
+  //   name: "description",
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you already have an image preview for this application?",
+  //   name: "hasImagePreview",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Path or URL of the image: ",
+  //   name: "imageURL",
+  //   when(answers) {
+  //     return answers.hasImagePreview;
+  //   },
+  // },
+  // {
+  //   type: "input",
+  //   message: "User story:",
+  //   name: "userStory",
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Are there any installation requirements for this application?",
+  //   name: "hasInstallationRequirements",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Installation requirements: ",
+  //   name: "installation",
+  //   when(answers) {
+  //     return answers.hasInstallationRequirements;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you provide tests for this application?",
+  //   name: "hasTests",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How to run the tests? ",
+  //   name: "tests",
+  //   when(answers) {
+  //     return answers.hasTests;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message:
+  //     "Do you want to add a credits section to list the resources and people that helped you throughout the project?",
+  //   name: "hasCredits",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Credits: ",
+  //   name: "credits",
+  //   when(answers) {
+  //     return answers.hasCredits;
+  //   },
+  // },
+  // {
+  //   type: "input",
+  //   message: "How can other developer contribute to the project? ",
+  //   name: "contributing",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Email address: ",
+  //   name: "emailAddress",
+  //   validate: isValidEmail,
+  // },
+  // {
+  //   type: "input",
+  //   message: "Github username: ",
+  //   name: "userName",
+  //   validate: isValidUserName,
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you use a license for this project?",
+  //   name: "hasLicense",
+  // },
+  // {
+  //   type: "list",
+  //   message: "Please select the license: ",
+  //   name: "licenseChoice",
+  //   choices: ["MIT License", "Apache 2.0 License", "The Unlicense"],
+  //   when(answers) {
+  //     return answers.hasLicense;
+  //   },
+  // },
 ];
 
 export { welcomeMsg, confirmInstructions, questions };
