addc0fcf01c3389bd7fee10757d70b886a4b454a
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-02-28 22:45:05 +0000

    Trim user emailAddress and imageURL

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -53,7 +53,9 @@ const createTableOfContent = () => {
 
 /* Description section */
 const generateImageURL = (dataImageURL) => {
-  return dataImageURL ? dataImageURL : "https://picsum.photos/id/1/800/500";
+  return dataImageURL
+    ? dataImageURL.trim()
+    : "https://picsum.photos/id/1/800/500";
 };
 
 const generateDescriptionSection = (dataDescription, imageURL) => {
@@ -187,7 +189,7 @@ const generateQuestionSection = (dataUserName, dataEmailAddress) => {
     : ".";
 
   const questions = `## ❔ QUESTIONS
-  - If you have any questions, [please click here to send me an email.](mailto:${dataEmailAddress}) 
+  - If you have any questions, [please click here to send me an email.](mailto:${dataEmailAddress.trim()}) 
   - You can also visit my Github profile ${userName}
   `;
 
@@ -197,7 +199,7 @@ const generateQuestionSection = (dataUserName, dataEmailAddress) => {
 const generateLicenseSection = (dataLicenseChoice) => {
   return dataLicenseChoice
     ? `## 📃 LICENSE  
-This application is covered under ${dataLicenseChoice}.`
+This application is covered under ${dataLicenseChoice.trim()}.`
     : `## 📃 LICENSE  
 This application doesn't have any license for the moment.`;
 };
